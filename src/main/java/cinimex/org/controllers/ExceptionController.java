37cdd6a21af6e97388ee56c8df44af0d6a5bfa2a
npotapov <potapovnik666@gmail.com>
2019-12-26 17:28:38 +0300

    Возвращён обработчик ошибок

--- src/main/java/cinimex/org/controllers/ExceptionController.java
+++ src/main/java/cinimex/org/controllers/ExceptionController.java
@@ -9,5 +9,16 @@ import javax.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class ExceptionController {
+    private static final String DEFAULT_ERROR_MESSAGE = "При обработке произошла непредвиденная ошибка.";
+
+    @ExceptionHandler(Exception.class)
+    public Response handleException(HttpServletRequest request, Exception exception) {
+        return Response.fail(DEFAULT_ERROR_MESSAGE);
+    }
+
+    @ExceptionHandler(LogicException.class)
+    public Response handleLogicException(HttpServletRequest request, LogicException exception) {
+        return Response.fail(exception.getMessage());
+    }
 
 }
